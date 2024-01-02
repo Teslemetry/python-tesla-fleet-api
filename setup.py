48dded805326da6c5133cdda7aba7fb44cec8fab
Brett Adams <Bre77@users.noreply.github.com>
2024-01-02 20:52:01 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tesla_fleep_api",
+    name="tesla_fleet_api",
     version="0.0.1",
     author="Brett Adams",
     author_email="hello@teslemetry.com",
