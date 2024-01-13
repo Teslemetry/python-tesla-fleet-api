7aa86b3d5f4c15c675b4a730c214d1986c16bba0
Brett <brett.whynot@gmail.com>
2024-01-13 13:43:19 +1000

    v0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tesla_fleet_api",
-    version="0.1.1",
+    version="0.1.4",
     author="Brett Adams",
     author_email="hello@teslemetry.com",
     description="Tesla Fleet API library for Python",
