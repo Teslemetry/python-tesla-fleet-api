3bb67a5ea6d83c1fc4684c3d37d65e2559ab04c4
Brett <brett.whynot@gmail.com>
2025-03-06 11:27:43 +1000

    v1.0.11

--- tesla_fleet_api/const.py
+++ tesla_fleet_api/const.py
@@ -3,7 +3,7 @@
 from enum import Enum
 import logging
 
-VERSION = "1.0.10"
+VERSION = "1.0.11"
 LOGGER = logging.getLogger(__package__)
 SERVERS = {
     "na": "https://fleet-api.prd.na.vn.cloud.tesla.com",
