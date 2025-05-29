823cf13a7b742b1d6db8e47c0379af2b03b1972e
Brett <brett.whynot@gmail.com>
2025-05-30 07:51:45 +1000

    v1.1.1

--- tesla_fleet_api/__init__.py
+++ tesla_fleet_api/__init__.py
@@ -1,7 +1,7 @@
 """Tesla Fleet API"""
 
 __author__ = "hello@teslemetry.com"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 from tesla_fleet_api.tesla.fleet import TeslaFleetApi
 from tesla_fleet_api.tesla.bluetooth import TeslaBluetooth
