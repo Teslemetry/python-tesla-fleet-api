2b8b246b8dc3f0d33fd82998e51556400098d330
Brett <brett.whynot@gmail.com>
2025-06-16 10:53:35 +1000

    Improved protoc upgrade script

--- upgradeProtoc.sh
+++ upgradeProtoc.sh
@@ -1,4 +1,8 @@
+PROTOC_VERSION=31.1
+
 protoc --version
-sudo unzip -o protoc-31.1-linux-x86_64.zip -d /usr/local bin/protoc
-sudo unzip -o protoc-31.1-linux-x86_64.zip -d /usr/local include/
+curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/protoc-$PROTOC_VERSION-linux-x86_64.zip
+sudo unzip -o protoc-$PROTOC_VERSION-linux-x86_64.zip -d /usr/local bin/protoc
+sudo unzip -o protoc-$PROTOC_VERSION-linux-x86_64.zip -d /usr/local 'include/*'
+rm protoc-$PROTOC_VERSION-linux-x86_64.zip
 protoc --version
