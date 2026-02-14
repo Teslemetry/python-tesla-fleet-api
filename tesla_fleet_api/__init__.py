fa9b5333a59510d1fc9ea7079293133ccf901f49
Brett Adams <brett.whynot@gmail.com>
2026-02-14 13:17:17 +1000

    v1.4.3

--- tesla_fleet_api/__init__.py
+++ tesla_fleet_api/__init__.py
@@ -1,7 +1,7 @@
 """Tesla Fleet API"""
 
 __author__ = "hello@teslemetry.com"
-__version__ = "1.4.1"
+__version__ = "1.4.3"
 
 from tesla_fleet_api.const import Region, is_valid_region
 from tesla_fleet_api.tesla.bluetooth import TeslaBluetooth
