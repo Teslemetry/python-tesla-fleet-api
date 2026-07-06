558bfc76405cb4f2dee617575601b42bdd832d5e
firstmate crewmate <crewmate@firstmate.local>
2026-07-07 07:12:19 +1000

    Bump version to 1.5.4

--- tesla_fleet_api/__init__.py
+++ tesla_fleet_api/__init__.py
@@ -1,7 +1,7 @@
 """Tesla Fleet API"""
 
 __author__ = "hello@teslemetry.com"
-__version__ = "1.5.3"
+__version__ = "1.5.4"
 
 from tesla_fleet_api.const import Region, is_valid_region
 from tesla_fleet_api.tesla.bluetooth import TeslaBluetooth
