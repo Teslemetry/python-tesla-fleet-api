dc116f0cb785390426d47b3c372b3eb36ab4834d
Brett Adams <Bre77@users.noreply.github.com>
2026-09-22 17:33:26 +1000

    Snapshot helper (#155)
    * First pass at a snapshot helper
    
    * Improvements

--- tesla_fleet_api/teslemetry/vehicle.py
+++ tesla_fleet_api/teslemetry/vehicle.py
@@ -18,6 +18,7 @@ from tesla_fleet_api.const import (
 from tesla_fleet_api.tesla.vehicle.vehicles import Vehicles
 from tesla_fleet_api.tesla.vehicle.fleet import VehicleFleet
 from tesla_fleet_api.const import LOGGER
+from tesla_fleet_api.teslemetry.const import SnapshotTopic
 
 if TYPE_CHECKING:
     from tesla_fleet_api.teslemetry.teslemetry import Teslemetry
@@ -644,6 +645,68 @@ class TeslemetryVehicle(VehicleFleet["Teslemetry"]):
             Method.POST,
             f"api/1/vehicles/{self.vin}/custom_command/set_rate_tariff",
         )
+
+    async def telemetry(
+        self,
+        topics: list[SnapshotTopic] | list[str] | str | None = None,
+        cursor: int | None = None,
+    ) -> dict[str, Any]:
+        """Gets a snapshot of cached vehicle telemetry"""
+
+        return await self._request(
+            Method.GET,
+            f"api/1/vehicles/{self.vin}/telemetry",
+            params={
+                "topics": ";".join(topics) if isinstance(topics, list) else topics,
+                "cursor": cursor
+            }
+        )
+
+    def create_telemetry_tracker(
+        self, topics: list[SnapshotTopic] | list[str] | str | None = None
+    ) -> TeslemetryVehicleTelemetry:
+        """Create a snapshot of cached vehicle telemetry tracker"""
+
+        return TeslemetryVehicleTelemetry(self, topics)
+
+class TeslemetryVehicleTelemetry():
+    """A cursor aware Vehicle Telemetry reader"""
+
+    _parent: TeslemetryVehicle
+    _topics: str | None
+    cursor: int = 0
+    snapshot: dict[str, Any]
+
+    def __init__(
+        self,
+        parent: TeslemetryVehicle,
+        topics: list[SnapshotTopic] | list[str] | str | None = None,
+    ) -> None:
+        self._parent = parent
+        self._topics = ";".join(topics) if isinstance(topics, list) else topics
+        self.snapshot = {}
+
+    async def update(self) -> dict[str, Any]:
+        """Get a fresh telemetry snapshot of changes."""
+        update = await self._parent.telemetry(self._topics, self.cursor)
+        if not update["response"]:
+            raise ValueError("No response from telemetry")
+        self.cursor = update["response"]["cursor"]
+        if update["response"].get("state"):
+            self.snapshot["state"] = update["response"]["state"]
+        if update["response"].get("data"):
+            self.snapshot.setdefault("data", {})
+            for field, data in update["response"]["data"].items():
+                self.snapshot["data"][field] = data
+        if update["response"].get("connectivity"):
+            self.snapshot["connectivity"] = update["response"]["connectivity"]
+        if update["response"].get("alerts"):
+            self.snapshot["alerts"] = update["response"]["alerts"]
+        if update["response"].get("errors"):
+            self.snapshot["errors"] = update["response"]["errors"]
+        if update["response"].get("vehicle_data"):
+            self.snapshot["vehicle_data"] = update["response"]["vehicle_data"]
+        return update["response"]
 
 
 class TeslemetryVehicles(Vehicles["Teslemetry"]):
